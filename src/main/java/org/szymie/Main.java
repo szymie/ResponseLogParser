64ba5f8060aa1b8b7dc7d4a9702f6e319a9832b4
Szymon Miężał <szymon.miezal@student.put.poznan.pl>
2017-09-08 16:22:58 +0200

    more stats.

--- src/main/java/org/szymie/Main.java
+++ src/main/java/org/szymie/Main.java
@@ -7,6 +7,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -26,8 +27,17 @@ public class Main {
         Stream<String> lines = Files.lines(Paths.get(inputFileName));
 
         Result resultAcc = lines.skip(1).map(line -> {
-            String result = line.split(",")[4];
-            return Arrays.stream(result.split(":")).collect(Collectors.toMap(o -> o.split("=")[0], o -> o.split("=")[1]));
+
+            String[] splitLine = line.split(",");
+            String result = splitLine[4];
+
+            Map<String, String> map = Arrays.stream(result.split(":")).collect(Collectors.toMap(o -> o.split("=")[0], o -> o.split("=")[1]));
+
+            map.put("start", splitLine[0]);
+            map.put("elapsed", splitLine[1]);
+            map.put("label", splitLine[2]);
+
+            return map;
         }).map(resultMap -> {
 
             long r = Long.parseLong(resultMap.get("r"));
@@ -36,17 +46,50 @@ public class Main {
             long a = Boolean.parseBoolean(resultMap.get("a")) ? 1 : 0;
             long at = Long.parseLong(resultMap.get("at"));
 
-            return new Result(1, r, mvr, tmvr, a, at, mvr > 0 ? 1 : 0);
-        }).reduce(new Result(0, 0, 0, 0, 0, 0, 0), (result1, result2) ->
-            new Result(result1.n + result2.n, result1.numberOfReads + result2.numberOfReads,
-                    result1.numberOfMultiValueReads + result2.numberOfMultiValueReads, result1.totalMultiValueReadSize + result2.totalMultiValueReadSize,
-                    result1.abort + result2.abort, result1.numberOfAttempts + result2.numberOfAttempts, result1.readMultiValue + result2.readMultiValue)
-        );
+            boolean ro = resultMap.get("label").startsWith("RO");
+            boolean rw = resultMap.get("label").startsWith("RW");
+            long start =  Long.parseLong(resultMap.get("start"));
+            long elapsed =  Long.parseLong(resultMap.get("elapsed"));
+
+            return new Result(1, r, mvr, tmvr, a, at, mvr > 0 ? 1 : 0, ro ? 1 : 0, rw ? 1 : 0, elapsed, start, 0, 0, 0, 0, ro ? elapsed : 0, rw ? elapsed : 0);
+        }).reduce(new Result(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), (acc, result) -> {
+
+            boolean ro = result.roCount > 0;
+            boolean rw = result.rwCount > 0;
+
+            long firstRoStartTime = acc.firstRoStartTime == 0 && ro ? result.startTime : acc.firstRoStartTime;
+            long firstRwStartTime = acc.firstRwStartTime == 0 && rw ? result.startTime : acc.firstRwStartTime;
+
+            long lastRoEndTime = ro ? result.startTime + result.elapsed : acc.lastRoEndTime;
+            long lastRwEndTime = rw ? result.startTime + result.elapsed : acc.lastRwEndTime;
+
+            return new Result(acc.n + result.n, acc.numberOfReads + result.numberOfReads,
+                    acc.numberOfMultiValueReads + result.numberOfMultiValueReads, acc.totalMultiValueReadSize + result.totalMultiValueReadSize,
+                    acc.abort + result.abort, acc.numberOfAttempts + result.numberOfAttempts, acc.readMultiValue + result.readMultiValue,
+                    acc.roCount + result.roCount, acc.rwCount + result.rwCount, 0, 0, firstRoStartTime, firstRwStartTime, lastRoEndTime, lastRwEndTime,
+                    acc.roTotalElapsed + (ro ? result.elapsed : 0), acc.rwTotalElapsed + (rw ? result.elapsed : 0));
+        });
 
         double abortRate = resultAcc.abort / (double) resultAcc.n;
         double multiValueReadsRate = resultAcc.numberOfMultiValueReads / (double) resultAcc.numberOfReads;
         double readMultiValueRate = resultAcc.readMultiValue / (double) resultAcc.n;
         double avgMultiValueReadSize = resultAcc.numberOfMultiValueReads != 0 ? resultAcc.totalMultiValueReadSize / (double) resultAcc.numberOfMultiValueReads : 0;
+
+        double roThroughput = resultAcc.roCount > 0 ? resultAcc.roCount / (double) (resultAcc.lastRoEndTime - resultAcc.firstRoStartTime) : 0;
+        double rwThroughput = resultAcc.rwCount > 0 ? resultAcc.rwCount / (double) (resultAcc.lastRwEndTime - resultAcc.firstRwStartTime) : 0;
+        double throughput;
+
+        if(roThroughput == 0) {
+            throughput = rwThroughput;
+        } else if(rwThroughput == 0) {
+            throughput = roThroughput;
+        } else {
+            throughput = (resultAcc.roCount + resultAcc.rwCount) / (double) (Math.max(resultAcc.lastRwEndTime, resultAcc.lastRoEndTime) - (Math.min(resultAcc.firstRoStartTime, resultAcc.firstRwStartTime)));
+        }
+
+        double avgRoResponseTime = resultAcc.roCount > 0 ? resultAcc.roTotalElapsed / resultAcc.roCount : 0;
+        double avgRwResponseTime = resultAcc.rwCount > 0 ? resultAcc.rwTotalElapsed / resultAcc.rwCount : 0;
+        double avgResponseTime = (resultAcc.roTotalElapsed + resultAcc.rwTotalElapsed) / (resultAcc.roCount + resultAcc.rwCount);
 
         List<String> results = Arrays.asList(
                 "Number of transactions: " + resultAcc.n,
@@ -55,7 +98,13 @@ public class Main {
                 "Abort rate: " + abortRate * 100 + "%",
                 "Multi value reads rate: " + multiValueReadsRate * 100 + "%",
                 "Transaction read multi value rate: " + readMultiValueRate * 100 + "%",
-                "Avg. multi value read size: " + avgMultiValueReadSize
+                "Avg. multi value read size: " + avgMultiValueReadSize,
+                "RO throughout: " + roThroughput * 1000 + "/sec",
+                "RW throughout: " + rwThroughput * 1000 + "/sec",
+                "Throughout: " + throughput * 1000 + "/sec",
+                "Avg. RO response time: " + avgRoResponseTime + " ms",
+                "Avg. RW response time: " + avgRwResponseTime + " ms",
+                "Avg. response time: " + avgResponseTime + " ms"
         );
 
         results.forEach(System.err::println);
--- src/main/java/org/szymie/Result.java
+++ src/main/java/org/szymie/Result.java
@@ -10,7 +10,21 @@ public class Result {
     long numberOfAttempts;
     long readMultiValue;
 
-    public Result(long n, long numberOfReads, long numberOfMultiValueReads, long totalMultiValueReadSize, long abort, long numberOfAttempts, long readMultiValue) {
+    long roCount;
+    long rwCount;
+
+    long elapsed;
+    long startTime;
+
+    long firstRoStartTime;
+    long firstRwStartTime;
+    long lastRoEndTime;
+    long lastRwEndTime;
+
+    long roTotalElapsed;
+    long rwTotalElapsed;
+
+    public Result(long n, long numberOfReads, long numberOfMultiValueReads, long totalMultiValueReadSize, long abort, long numberOfAttempts, long readMultiValue, long roCount, long rwCount, long elapsed, long startTime, long firstRoStartTime, long firstRwStartTime, long lastRoEndTime, long lastRwEndTime, long roTotalElapsed, long rwTotalElapsed) {
         this.n = n;
         this.numberOfReads = numberOfReads;
         this.numberOfMultiValueReads = numberOfMultiValueReads;
@@ -18,5 +32,15 @@ public class Result {
         this.abort = abort;
         this.numberOfAttempts = numberOfAttempts;
         this.readMultiValue = readMultiValue;
+        this.roCount = roCount;
+        this.rwCount = rwCount;
+        this.elapsed = elapsed;
+        this.startTime = startTime;
+        this.firstRoStartTime = firstRoStartTime;
+        this.firstRwStartTime = firstRwStartTime;
+        this.lastRoEndTime = lastRoEndTime;
+        this.lastRwEndTime = lastRwEndTime;
+        this.roTotalElapsed = roTotalElapsed;
+        this.rwTotalElapsed = rwTotalElapsed;
     }
 }
